1ff0678aba8cc378ee68650aeffff7a116707f9f
Miguel Palhas <mpalhas@gmail.com>
2021-10-04 08:34:37 +0100

    Changing keyboard shorcuts

--- keymap.c
+++ keymap.c
@@ -44,9 +44,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
   // left hand
   KC_ESC,     KC_1,    KC_2,     KC_3,     KC_4,   KC_5,  LANG_SWITCH,
-  KC_TAB,     KC_Q,    KC_W,     KC_E,     KC_R,   KC_T,  KC_F2,
+  KC_TAB,     KC_Q,    KC_W,     KC_E,     KC_R,   KC_T,  LAG(KC_U),
   KC_LCTL,    KC_A,    KC_S,     KC_D,     KC_F,   KC_G,
-  KC_LSFT,    KC_Z,    KC_X,     KC_C,     KC_V,   KC_B,  KC_F3,
+  KC_LSFT,    KC_Z,    KC_X,     KC_C,     KC_V,   KC_B,  LAG(KC_I),
   MO(1),      KC_GRV,  KC_QUOT,  KC_F1,  MO(2),
 
                                                KC_HOME,   KC_END,
