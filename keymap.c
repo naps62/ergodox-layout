d033d9b1ac424a4d394fcb9ba59d64b30d7c32b8
Miguel Palhas <mpalhas@gmail.com>
2022-02-28 18:53:13 +0000

    Small update

--- keymap.c
+++ keymap.c
@@ -18,6 +18,7 @@ enum custom_keycodes {
 };
 
 #define LANG_SWITCH LALT(KC_LSFT)
+#define CTRL_BACKTICK LCTL(KC_GRV)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
@@ -46,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,     KC_1,    KC_2,     KC_3,     KC_4,   KC_5,  LANG_SWITCH,
   KC_TAB,     KC_Q,    KC_W,     KC_E,     KC_R,   KC_T,  LAG(KC_U),
   KC_LCTL,    KC_A,    KC_S,     KC_D,     KC_F,   KC_G,
-  KC_LSFT,    KC_Z,    KC_X,     KC_C,     KC_V,   KC_B,  LAG(KC_I),
+  KC_LSFT,    KC_Z,    KC_X,     KC_C,     KC_V,   KC_B,  CTRL_BACKTICK,
   MO(1),      KC_GRV,  KC_QUOT,  KC_F1,  MO(2),
 
                                                KC_HOME,   KC_END,
